8573342b23b902fcb31a46a5678dd30dc1715192
DavidDov <109440152+Daviddov@users.noreply.github.com>
2023-08-01 13:06:44 +0300

    Update Header.h

--- Header.h
+++ Header.h
@@ -2,13 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "opencv2/core/mat.hpp"
-#include "opencv2/calib3d/calib3d.hpp"
 #include <opencv2/opencv.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/imgcodecs.hpp >
 #include <filesystem>
 #include <vector>
 #include <queue>
