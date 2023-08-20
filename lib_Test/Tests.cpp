8df81c391117dd8f526d0020f3129570f7ec5d48
Dovbar <baror2009@gmail.com>
2023-08-20 11:16:02 +0300

    m

--- lib_Test/Tests.cpp
+++ lib_Test/Tests.cpp
@@ -56,27 +56,34 @@ TEST(CalcAbsDiffTest, TestCalcAbsDiff) {
     ASSERT_TRUE(camera.calcAbsDiff());
 }
 
+
 TEST(CurrentTimeTest, TestCurrentTime) {
     // Get the current system time using the C++ standard library
-    auto now = chrono::system_clock::now();
-    std::time_t timestamp = chrono::system_clock::to_time_t(now);
+    auto now = std::chrono::system_clock::now();
+    std::time_t timestamp = std::chrono::system_clock::to_time_t(now);
     std::tm timeinfo;
-    localtime_s(&timeinfo, &timestamp);
 
+    // Use thread-safe version of localtime
+    localtime_s(&timeinfo, &timestamp);
 
     char buffer[28];
     strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", &timeinfo);
 
+    // Calculate milliseconds
+    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count() % 1000;
+    snprintf(buffer + strlen(buffer), sizeof(buffer) - strlen(buffer), ":%03d", static_cast<int>(milliseconds));
 
-    auto milliseconds = chrono::duration_cast<chrono::milliseconds>(now.time_since_epoch()).count() % 1000;
-    sprintf_s(buffer + strlen(buffer), sizeof(buffer) - strlen(buffer), ":%03d", static_cast<int>(milliseconds));
-
-    string expected_formatted_time = buffer;
-
+    std::string expected_formatted_time = buffer;
 
     // Call the currentTime function to get the result
-    string actual_formatted_time = currentTime();
+    std::string actual_formatted_time = currentTime();
 
-    // Check if the result matches the expected formatted time
-    ASSERT_EQ(expected_formatted_time, actual_formatted_time);
+    // Calculate the absolute difference in milliseconds
+    int expected_milliseconds = std::stoi(expected_formatted_time.substr(20, 3));
+    int actual_milliseconds = std::stoi(actual_formatted_time.substr(20, 3));
+    int time_difference = std::abs(expected_milliseconds - actual_milliseconds);
+
+    // Check if the time difference is less than 5 milliseconds
+    ASSERT_LE(time_difference, 5);
 }
+
